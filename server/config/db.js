7185d985175321023c14f741fe5dda68eabf999f
lasitharathnayaka <83506365+lasitharathnayaka@users.noreply.github.com>
2026-03-29 20:35:14 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGO_URI || 'mongodb+srv://db_user:tharaka123@cluster0.xhawcbe.mongodb.net/?appName=Cluster0'
+      process.env.MONGO_URI || 'mongodb://localhost:27017/unihelp'
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
@@ -13,5 +13,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-//mongodb+srv://db_user:tharaka123@cluster0.xhawcbe.mongodb.net/?appName=Cluster0
 //mongodb://localhost:27017/unihelp
