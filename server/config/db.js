683e9313e545afe6c8c375d7af66885d04d7e50e
lasitharathnayaka <newlasitharathnayaka580@gmail.com>
2026-04-23 16:31:42 +0530

    Update MongoDB connection to local and sync job UI improvements

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGO_URI || 'mongodb+srv://db_user:tharaka123@cluster0.xhawcbe.mongodb.net/unihelp?retryWrites=true&w=majority'
+      process.env.MONGO_URI || 'mongodb://localhost:27017/unihelp'
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
